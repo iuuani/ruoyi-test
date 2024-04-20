c0add2f2a93a930503d34dd784a176a5b4a3e7a6
dongysuhu123 <1322816498@qq.com>
2024-04-20 20:07:45 +0800

    创建了新模块

---
+++ ruoyi-ui/src/views/Newmodule/list/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        新模块
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
